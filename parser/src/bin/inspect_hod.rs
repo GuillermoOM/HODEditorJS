12b44ca20d2da4c1caad0bfd9d88f2388739762e
GuillermoOM <guille8ameou@gmail.com>
2026-06-03 12:46:06 -0700

    chore(parser): Add inspect_hod utility script for dumping HOD structures
    - Handy for inspecting legacy HODs and understanding their internal texture/material groupings.

--- parser/src/bin/inspect_hod.rs
+++ parser/src/bin/inspect_hod.rs
@@ -1,14 +1,14 @@
 use std::fs::File;
 use std::io::Read;
-use hwr_hod_parser::hod::Hod;
+use hwr_hod_parser::hod::HODModel;
 
 fn main() {
-    let path = "../../testing/ter_fenris/ter_fenris_1.3_original.hod";
+    let path = "/run/media/system/Data/SteamLibrary/steamapps/common/Homeworld 347380/GBXTools/WorkshopTool/mod-tools/HODEditorJS/testing/ter_fenris/ter_fenris_1.3_original.hod";
     let mut file = File::open(path).expect("Failed to open HOD");
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).expect("Failed to read");
     
-    let hod = Hod::deserialize(&buffer).expect("Failed to parse");
+    let hod = HODModel::parse(&buffer).expect("Failed to parse");
     
     println!("Textures:");
     for tex in hod.textures {
@@ -17,6 +17,6 @@ fn main() {
     
     println!("\nMaterials:");
     for mat in hod.materials {
-        println!(" - {}: shader={}, maps={:?}", mat.name, mat.shader, mat.texture_maps);
+        println!(" - {}: shader={}, maps={:?}", mat.name, mat.shader_name, mat.texture_maps);
     }
 }
